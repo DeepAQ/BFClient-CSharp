addfdfe51586923c7cbb840cd3a1eccd6a0dbff4
DeepAQ <i@aq741.com>
2016-07-29 23:46:49 +0800

    FileOpenForm

---
+++ BFClient-CSharp/View/FileOpenForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace BFClient_CSharp.View
+{
+    partial class FileOpenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listFile = new System.Windows.Forms.ListView();
+            this.listVersion = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.columnFilename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnLatestVersion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnVersion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listFile
+            // 
+            this.listFile.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnFilename,
+            this.columnLatestVersion});
+            this.listFile.Location = new System.Drawing.Point(20, 30);
+            this.listFile.Name = "listFile";
+            this.listFile.Size = new System.Drawing.Size(270, 360);
+            this.listFile.TabIndex = 0;
+            this.listFile.UseCompatibleStateImageBehavior = false;
+            // 
+            // listVersion
+            // 
+            this.listVersion.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnVersion});
+            this.listVersion.Location = new System.Drawing.Point(300, 30);
+            this.listVersion.Name = "listVersion";
+            this.listVersion.Size = new System.Drawing.Size(170, 360);
+            this.listVersion.TabIndex = 1;
+            this.listVersion.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select a file :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select a version :";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(314, 401);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(395, 401);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // columnFilename
+            // 
+            this.columnFilename.Text = "File Name";
+            this.columnFilename.Width = 150;
+            // 
+            // columnLatestVersion
+            // 
+            this.columnLatestVersion.Text = "Latest Version";
+            this.columnLatestVersion.Width = 100;
+            // 
+            // columnVersion
+            // 
+            this.columnVersion.Text = "Version";
+            this.columnVersion.Width = 150;
+            // 
+            // FileOpenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 434);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listVersion);
+            this.Controls.Add(this.listFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FileOpenForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileOpenForm";
+            this.Shown += new System.EventHandler(this.FileOpenForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listFile;
+        private System.Windows.Forms.ListView listVersion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ColumnHeader columnFilename;
+        private System.Windows.Forms.ColumnHeader columnLatestVersion;
+        private System.Windows.Forms.ColumnHeader columnVersion;
+    }
+}
---
+++ BFClient-CSharp/View/FileOpenForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BFClient_CSharp.View
+{
+    public partial class FileOpenForm : Form
+    {
+        public FileOpenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FileOpenForm_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
